5039e1ea849430a1c8803ee741616240a0e8b015
adewalekujore <adewalekujore6@gmail.com>
2024-07-08 09:57:26 +0100

    silly people

--- src/main/java/com/hngstagetwo/dtos/UserResponseDto.java
+++ src/main/java/com/hngstagetwo/dtos/UserResponseDto.java
@@ -1,10 +1,12 @@
 package com.hngstagetwo.dtos;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class UserResponseDto {
     String userId;
     String firstName;
